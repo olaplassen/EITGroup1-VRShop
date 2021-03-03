835d37928528d2f02c6f9e77b18a69c9cb47691d
Ola Plassen <okp.97@hotmail.com>
2021-03-03 12:38:51 +0100

    interactions

--- Assets/LocomotionController.cs
+++ Assets/LocomotionController.cs
@@ -9,18 +9,20 @@ public class LocomotionController : MonoBehaviour
     public XRController leftRay, rightRay;
     public InputHelpers.Button teleportButton;
     public float activationTreshold = 0.1f;
-    
+
+    public bool EnableLeftTeleport { get; set;  } = true;
+    public bool EnableRightTeleport { get; set; } = true;
 
     // Update is called once per frame
     void Update()
     {
         if (leftRay)
         {
-            leftRay.gameObject.SetActive(CheckIfActivated(leftRay));
+            leftRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftRay));
         }
         if (rightRay)
         {
-            rightRay.gameObject.SetActive(CheckIfActivated(rightRay));
+            rightRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightRay));
         }
     }
 
---
+++ Assets/XROffsetGrabInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    protected override void OnSelectEntering(XRBaseInteractor interactor)
+    {
+        if(interactor is XRDirectInteractor)
+        {
+            attachTransform.position = interactor.transform.position;
+            attachTransform.rotation = interactor.transform.rotation;
+        }
+
+        base.OnSelectEntering(interactor);
+    }
+}
